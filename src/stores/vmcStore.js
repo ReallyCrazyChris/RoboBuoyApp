1f795cd4ab084b49e37f4bf1c9ad432cf3f6dfef
Chris Bentley <chris.bentley@siemens-healthineers.com>
2024-06-02 14:20:05 +0200

    working on vmc calculation

--- src/stores/vmcStore.js
+++ src/stores/vmcStore.js
@@ -1,9 +1,16 @@
 import { defineStore } from "pinia";
+import { Loading } from "quasar";
 
 export const useVmcStore = defineStore("vmcStore", {
   state: () => ({
     vmc: 1,
     sog: 2,
     risk: 55,
+    lon: 0,
+    lat: 0,
+    bearing: 0,
   }),
+  actions: {
+    update(lat, lon, heading, speed) {},
+  },
 });
