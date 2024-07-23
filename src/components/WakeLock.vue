65d8e65fa9051433bc20e849c9ced0fb419aa949
Chris Bentley <chris.bentley@siemens-healthineers.com>
2024-07-23 12:54:41 +0200

    test wakelock

--- src/components/WakeLock.vue
+++ src/components/WakeLock.vue
@@ -1,6 +1,23 @@
 <template>
-  <q-btn round push color="primary" icon="lock" @click="requestWakeLock()">
+  <q-btn
+    v-if="!wakeLockActivated"
+    round
+    push
+    color="primary"
+    icon="lock_open"
+    @click="requestWakeLock()"
+  >
     <q-tooltip class="primary">Wake Lock</q-tooltip>
+  </q-btn>
+  <q-btn
+    v-if="wakeLockActivated"
+    round
+    push
+    color="primary"
+    icon="lock"
+    @click="requestWakeLock()"
+  >
+    <q-tooltip class="primary">Release Wake Lock</q-tooltip>
   </q-btn>
 </template>
 
@@ -10,6 +27,7 @@ export default {
   data() {
     return {
       wakeLock: undefined,
+      wakeLockActivated: false,
     };
   },
   setup() {
@@ -21,11 +39,14 @@ export default {
         try {
           this.wakeLock = await navigator.wakeLock.request("screen");
           console.log("ACTIVATED");
+          this.wakeLockActivated = true;
         } catch (err) {
           console.log("ERROR:", err);
+          this.wakeLockActivated = false;
         }
       } else {
         console.log("wakeLock not available");
+        this.wakeLockActivated = false;
       }
     },
   },
