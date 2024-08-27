4bac68bf9d0ce816082d9a83cf59f4873555bffd
Chris Bentley <chris.bentley@siemens-healthineers.com>
2024-08-27 14:06:30 +0200

    vmc pin  working again

--- src/pages/VmcPage.vue
+++ src/pages/VmcPage.vue
@@ -15,15 +15,20 @@
 
       <q-card-section>
         <div class="row">
-          <div class="col-5">
-            <div class="text-caption">
-              N {{ parseFloat(vmc.lat).toFixed(3) }}
-            </div>
-            <div class="text-caption">
-              E {{ parseFloat(vmc.lon).toFixed(3) }}
+          <div class="col-8">
+            <div class="row">
+              <div class="col-12 self-center">
+                Pin the location of the vmc buoy
+              </div>
+              <div class="col-6 text-caption">
+                N {{ parseFloat(vmc.lat).toFixed(3) }}
+              </div>
+              <div class="col-6 text-caption">
+                E {{ parseFloat(vmc.lon).toFixed(3) }}
+              </div>
             </div>
           </div>
-          <div class="col-7 self-center">
+          <div class="col-4 self-center text-right">
             <q-btn
               round
               color="primary"
@@ -45,11 +50,16 @@ import sogview from "src/components/boat/sog.vue";
 import vmcview from "src/components/boat/vmc.vue";
 import efficiencyview from "src/components/boat/efficiency.vue";
 
+import { useGps } from "src/stores/gps";
+import { useVmc } from "src/stores/vmc";
+const gps = useGps();
+const vmc = useVmc();
+
 export default defineComponent({
   name: "VmcPage",
   components: { sogview, vmcview, efficiencyview },
   setup() {
-    return {};
+    return { gps, vmc };
   },
 });
 </script>
