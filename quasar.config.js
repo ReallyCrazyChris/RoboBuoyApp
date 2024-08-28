ab0ba5c1ab5ec053aea770ee499e56c6e651d195
Chris Bentley <chris.bentley@siemens-healthineers.com>
2024-08-28 10:42:59 +0200

    preparing for initial pwa

--- quasar.config.js
+++ quasar.config.js
@@ -212,7 +212,7 @@ module.exports = configure(function (/* ctx */) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: "robobuoy",
+        appId: "quickregatta",
       },
     },
 
