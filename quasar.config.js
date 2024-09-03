ee139e51fa4d3e0cfa6c25e8365d1a2adfa4f185
Chris Bentley <chris.bentley@siemens-healthineers.com>
2024-09-03 14:27:21 +0200

    simplifications

--- quasar.config.js
+++ quasar.config.js
@@ -212,7 +212,7 @@ module.exports = configure(function (/* ctx */) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: "quickregatta",
+        appId: "quichrace",
       },
     },
 
